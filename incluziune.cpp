af8d57d97e0a3e0ab187d157f99fd593b3a7a1a5
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-16 08:38:30 +0300

    refactor

--- incluziune.cpp
+++ incluziune.cpp
@@ -1,13 +1,11 @@
 #include <fstream>
+#define CMAX_ASCII 256
 using namespace std;
 ifstream fin("prim.txt");
 ofstream fout("doi.txt");
-struct multime
-{
-	int faz[26], fAZ[26], f09[10], nr;
-};
-void initializeazaMultimea(multime &);
-void citesteMultimea(multime &);
+typedef int multime[CMAX_ASCII];
+void initializeazaMultimea(multime);
+void citesteMultimea(multime);
 bool incluziune(multime, multime);
 int main()
 {
@@ -16,6 +14,7 @@ int main()
 	initializeazaMultimea(B);
 	citesteMultimea(A);
 	citesteMultimea(B);
+	fin.close();
 	if (incluziune(A, B))
 	{
 		fout << "DA";
@@ -24,21 +23,17 @@ int main()
 	{
 		fout << "NU";
 	}
+	fout.close();
 	return 0;
 }
-void initializeazaMultimea(multime & M)
+void initializeazaMultimea(multime M)
 {
-	for (int i = 0; i < 26; i++)
+	for (int i = 0; i < CMAX_ASCII; i++)
 	{
-		M.faz[i] = M.fAZ[i] = 0;
-		if (i < 10)
-		{
-			M.f09[i] = 0;
-		}
+		M[i] = 0;
 	}
-	M.nr = 0;
 }
-void citesteMultimea(multime & M)
+void citesteMultimea(multime M)
 {
 	char c;
 	do
@@ -46,46 +41,19 @@ void citesteMultimea(multime & M)
 		fin.get(c);
 		if (c != '\n' && c != EOF)
 		{
-			if (c >= 'a' && c <= 'z')
-			{
-				M.nr++;
-				M.faz[c-'a']++;
-			}
-			else if (c >= 'A' && c <= 'Z')
-			{
-				M.nr++;
-				M.fAZ[c-'A']++;
-			}
-			else if (c >= '0' && c <= '9')
-			{
-				M.nr++;
-				M.f09[c-'0']++;
-			}
+			M[c] = 1;
 		}
 	} while (c != '\n' && c != EOF);
 }
 // verifica daca multimea A este inclusa in multimea B
 bool incluziune(multime A, multime B)
 {
-	int i, nr;
-	for (nr = i = 0; i < 26; i++)
+	for (int i = 0; i < CMAX_ASCII; i++)
 	{
-		if (i < 10 && B.f09[i] && A.f09[i])
+		if (A[i] > B[i])
 		{
-			nr++;
-		}
-		if (B.faz[i] && A.faz[i])
-		{
-			nr++;
-		}
-		if (B.fAZ[i] && A.fAZ[i])
-		{
-			nr++;
+			return 0;
 		}
 	}
-	if (nr == A.nr)
-	{
-		return 1;
-	}
-	return 0;
+	return 1;
 }
